a91711aa0c5933e3becd6221bf1be4042711ee75
Amel Mennad <amennad42nice@gmail.com>
2023-04-26 17:29:26 +0200

    day 2 no true work

---
+++ test/base1.c
@@ -0,0 +1,24 @@
+
+
+char	*get_next_line(int fd)
+{
+	printf("fd gnl file : %d\n", fd);
+	int		buffer_size;
+	int		retrieve;
+	char	*buffer;
+	// char	*temp;
+	// int 	i;
+
+	// buffer_size = 2;
+	// temp = (char *)malloc(sizeof(char) * buffer_size);
+	// buffer = (char *)malloc(sizeof(char) * (buffer_size * i));
+	// while ((retrieve = read(fd, buffer, buffer_size)) == buffer_size)
+	// {
+	// 	free(buffer);
+	// 	printf("retrieve : %d\n", retrieve);
+	// 	printf("buffer : %s\n", buffer);
+	// 	printf("i : %d\n", i);
+	// 	i++;
+	// }
+	return (0);
+}
