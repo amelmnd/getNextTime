4e6655e91130a437b7907c6e936dcc3f366c96ab
amelmnd <amelmnd.dev@gmail.com>
2023-04-28 12:10:13 +0200

    gnl : return online line

--- getNextLine/main.c
+++ getNextLine/main.c
@@ -6,7 +6,7 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:01:55 by amennad           #+#    #+#             */
-/*   Updated: 2023/04/26 13:19:47 by amennad          ###   ########.fr       */
+/*   Updated: 2023/04/28 12:01:41 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@ int	main(void)
 	int		fd;
 	char	*result;
 
-	fd = open("oneLineOneWorld", O_RDONLY);
+	fd = open("oneLineMoreWorlds", O_RDONLY);
 	// fd = open("oneLineMoreWorlds", O_RDONLY);
 	// fd = open("lorem", O_RDONLY);
 	if (fd == -1)
 		printf("open() failed\n");
 	printf("fd main file : %d\n", fd);
 	result = get_next_line(fd);
-	printf("%s\n", result);
+	if (result)
+		printf("%s\n", result);
 	return (0);
 }
