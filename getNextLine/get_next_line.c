d1002f156cbf5408fb95b6623669acdde5a22fd8
amelmnd <amelmnd.dev@gmail.com>
2023-05-05 11:52:39 +0200

    gnl split working

--- getNextLine/get_next_line.c
+++ getNextLine/get_next_line.c
@@ -6,36 +6,18 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:13:01 by amennad           #+#    #+#             */
-/*   Updated: 2023/05/05 10:46:27 by amennad          ###   ########.fr       */
+/*   Updated: 2023/05/05 11:51:42 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*test(int fd, char *buffer, char *line)
 {
-	static char	*buffer;
-	char		*line;
-	int			i;
-	int			read_nb_cara;
-	int			line_break;
+	int	i;
+	int	read_nb_cara;
 
 	read_nb_cara = BUFFER_SIZE;
-
-	if (fd == -1 || BUFFER_SIZE <= 0)
-		return (NULL);
-	printf("____BUFFER : %s____", buffer);
-	if (buffer)
-		line = ft_strdup(buffer);
-	else
-	{
-		buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-		if (!buffer)
-			return (NULL);
-		line = NULL;
-	}
-	printf("____LINE : %s____", line);
-
 	while (read_nb_cara == BUFFER_SIZE)
 	{
 		read_nb_cara = read(fd, buffer, BUFFER_SIZE);
@@ -44,7 +26,6 @@ char	*get_next_line(int fd)
 		if (read_nb_cara == -1)
 			return (NULL);
 		i = 0;
-		line_break = 0;
 		while (i < read_nb_cara)
 		{
 			if (buffer[i] == '\n')
@@ -64,5 +45,34 @@ char	*get_next_line(int fd)
 			line = ft_strjoin(line, ft_substr(buffer, 0, read_nb_cara));
 		printf("LINE : %s--\n", line);
 	}
+	return (line);
+
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+	// int			i;
+	//int			read_nb_cara;
+
+	// read_nb_cara = BUFFER_SIZE;
+	if (fd == -1 || BUFFER_SIZE <= 0)
+		return (NULL);
+	printf("____BUFFER : %s____\n", buffer);
+	if (buffer)
+		line = ft_strdup(buffer);
+	else
+	{
+		buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+		if (!buffer)
+			return (NULL);
+		line = NULL;
+	}
+	printf("____LINE : %s____\n", line);
+
+
+	line = test(fd, buffer, line);
+
 	return (line);
 }
