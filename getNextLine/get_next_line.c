227e55770cc149ed47730e5e764b21c59d4d12fa
amelmnd <amelmnd.dev@gmail.com>
2023-05-05 10:48:42 +0200

    gnl working

--- getNextLine/get_next_line.c
+++ getNextLine/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:13:01 by amennad           #+#    #+#             */
-/*   Updated: 2023/05/03 14:28:44 by amennad          ###   ########.fr       */
+/*   Updated: 2023/05/05 10:46:27 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,49 +17,51 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 	int			i;
-	static int	nb_car_lu;
+	int			read_nb_cara;
+	int			line_break;
 
-	int line_break ;
-	nb_car_lu = BUFFER_SIZE;
+	read_nb_cara = BUFFER_SIZE;
 
-	if (fd == -1)
+	if (fd == -1 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!buffer)
-		return (NULL);
-	if (buffer[0])
+	printf("____BUFFER : %s____", buffer);
+	if (buffer)
 		line = ft_strdup(buffer);
 	else
-		line = NULL;
-	while (nb_car_lu == BUFFER_SIZE)
 	{
-		nb_car_lu = read(fd, buffer, BUFFER_SIZE);
-		buffer[nb_car_lu] = '\0';
-		if (nb_car_lu == -1)
+		buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+		if (!buffer)
 			return (NULL);
-		printf("nb_car_lu %d\n", nb_car_lu);
+		line = NULL;
+	}
+	printf("____LINE : %s____", line);
+
+	while (read_nb_cara == BUFFER_SIZE)
+	{
+		read_nb_cara = read(fd, buffer, BUFFER_SIZE);
+		printf("buffer 1 : %s\n", buffer);
+		buffer[read_nb_cara] = '\0';
+		if (read_nb_cara == -1)
+			return (NULL);
 		i = 0;
 		line_break = 0;
-		while (i < nb_car_lu)
+		while (i < read_nb_cara)
 		{
-			if (buffer[i] == '\n' || (buffer[i] == '\\' && buffer[i
-					+ 1] == 'n'))
+			if (buffer[i] == '\n')
 			{
 				line = ft_strjoin(line, ft_substr(buffer, 0, i));
-				buffer = ft_substr(buffer, ++i, nb_car_lu);
+				printf("---- line : %s------\n", line);
+				buffer = ft_substr(buffer, ++i, read_nb_cara);
+				printf("---- BUFFER : %s------\n", buffer);
 				return (line);
 			}
 			i++;
 		}
-		if (!line_break)
-			printf("no line_break");
-		else
-			printf("line_break\n");
 		printf("BUFFER : %s--\n", buffer);
 		if (!line)
 			line = ft_strdup(buffer);
 		else
-			line = ft_strjoin(line, ft_substr(buffer, 0, nb_car_lu));
+			line = ft_strjoin(line, ft_substr(buffer, 0, read_nb_cara));
 		printf("LINE : %s--\n", line);
 	}
 	return (line);
