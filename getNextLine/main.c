8769b2dd0784076440cd8cdda8846ed735fd2ce9
amelmnd <amelmnd.dev@gmail.com>
2023-05-05 15:59:44 +0200

    gnl facto and work but write only first line

--- getNextLine/main.c
+++ getNextLine/main.c
@@ -6,7 +6,7 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:01:55 by amennad           #+#    #+#             */
-/*   Updated: 2023/05/03 14:16:02 by amennad          ###   ########.fr       */
+/*   Updated: 2023/05/05 13:39:57 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,6 @@ int	main(void)
 	result1 = get_next_line(fd);
 	result2 = get_next_line(fd);
 	printf("|result1 : %s|\n", result1);
-	printf("|result2 : %s|\n", result2);
+	// printf("|result2 : %s|\n", result2);
 	return (0);
 }
