56920f71253d969090a7d90a45439d497fd8af13
amelmnd <amelmnd.dev@gmail.com>
2023-05-12 16:08:24 +0200

    gnf write 1 line, no leak

--- getNextLine/get_next_line.c
+++ getNextLine/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:13:01 by amennad           #+#    #+#             */
-/*   Updated: 2023/05/12 15:12:13 by amennad          ###   ########.fr       */
+/*   Updated: 2023/05/12 16:05:46 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ static char	*read_line(fd)
 	char	*buffer;
 	char	*line;
 	int		nb_cara_read;
+	// char	*temp;
 
 	nb_cara_read = BUFFER_SIZE;
 	buffer = (char *)ft_calloc(BUFFER_SIZE, sizeof(char));
