961f09b0594f4d9f0741d2aa91eef2a75b5e7ec2
amelmnd <amelmnd.dev@gmail.com>
2023-05-17 12:38:44 +0200

    gnl step more

--- getNextLine/get_next_line.c
+++ getNextLine/get_next_line.c
@@ -6,34 +6,18 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:13:01 by amennad           #+#    #+#             */
-/*   Updated: 2023/05/16 18:19:32 by amennad          ###   ########.fr       */
+/*   Updated: 2023/05/17 12:37:29 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// static char	*check_buffer(char *buffer, char *line, int nb_cara_read)
-// {
-// 	int	i;
-// 	i = 0;
-// 	if (buffer[i] == '\n')
-// 	{
-// 		buffer[i] = 'T';
-// 		line = ft_strjoin(line, ft_substr(buffer, 0, i));
-// 		buffer = ft_substr(buffer, i + 1, nb_cara_read);
-// 		printf("**REST : %s**\n", buffer);
-// 		free(buffer); //eviter les leaks mais non vaible
-// 		return (line);
-// 	}
-// 	return (0);
-// }
 
 static char	*read_line(int fd, char *buffer, char *line)
 {
 	int			nb_cara_read;
 	int			i;
 	static char	*temp;
-	printf("**buffer : %p**\n", buffer) ;
 
 	if (temp)
 	{
@@ -64,21 +48,14 @@ static char	*read_line(int fd, char *buffer, char *line)
 					return (NULL);
 				}
 				temp = ft_substr(buffer, i + 1, nb_cara_read);
-					printf("**temp : %p**\n", temp) ;
 
 				line = ft_strjoin(line, ft_substr(buffer, 0, i));
-					printf("**line : %p**\n", line) ;
-					free(buffer);
 				return (line);
 			}
 			i++;
 		}
-
 		line = ft_strjoin(line, ft_substr(buffer, 0, nb_cara_read));
-		printf("**line 2 : %p**\n", line) ;
-
 	}
-	free(buffer);
 	return (line);
 }
 
@@ -99,6 +76,6 @@ char	*get_next_line(int fd)
 		return (NULL);
 	}
 	line = read_line(fd, buffer, line);
-	// free(buffer);
+	free(buffer);
 	return (line);
 }
