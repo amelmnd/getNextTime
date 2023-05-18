1d51f8a422afa6e71324568a8fa9260e2500e4f9
amelmnd <amelmnd.dev@gmail.com>
2023-05-18 16:53:31 +0200

    intermediaire step

--- getNextLine/get_next_line.c
+++ getNextLine/get_next_line.c
@@ -6,67 +6,60 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:13:01 by amennad           #+#    #+#             */
-/*   Updated: 2023/05/18 14:42:19 by amennad          ###   ########.fr       */
+/*   Updated: 2023/05/18 16:51:33 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// static char	*check_buffer(int nb_cara_read, char *buffer, char *temp,
-// 		char *line)
-static void	check_buffer(int nb_cara_read, char *buffer)
+
+static char	*check_buffer(int nb_cara_read, char *buffer, char *line, char **temp)
 {
 	int	i;
-	int	y;
-	int	z;
 
 	i = 0;
-	y = 0;
-	z = 0;
 	while (i <= nb_cara_read)
 	{
-		y++;
 		if (buffer[i] == '\n')
 		{
-			printf("\n- %s\n", ft_substr(buffer, z, y));
-			y = 0;
-			z = i + 1;
+			if (!*temp)
+				*temp = malloc(sizeof(char) * (nb_cara_read - i));
+			if (!*temp)
+			{
+				free(line);
+				return (NULL);
+			}
+			*temp = ft_substr(buffer, i, nb_cara_read);
+			line = ft_strjoin(line, ft_substr(buffer, 0, i + 1));
+			return (line);
 		}
 		else if (i == nb_cara_read && nb_cara_read < BUFFER_SIZE)
 		{
-			printf("\n- %s\n", ft_substr(buffer, 0, nb_cara_read));
+			line = ft_substr(buffer, 0, nb_cara_read);
+			return (line);
 		}
-		// if (buffer[i] == '\n' && buffer[i + 1] != '\n')
-		// {
-		// 	temp = malloc(sizeof(char) * (nb_cara_read - i));
-		// 	if (!temp)
-		// 	{
-		// 		free(line);
-		// 		return (NULL);
-		// 	}
-		// 	temp = ft_substr(buffer, i + 1, nb_cara_read);
-		// 	line = ft_strjoin(line, ft_substr(buffer, 0, i));
-		// 	return (line);
-		// }
 		i++;
 	}
+	line = ft_strjoin(line, ft_substr(buffer, 0, nb_cara_read));
+	return (NULL);
 }
 
 static char	*read_line(int fd, char *buffer, char *line)
 {
 	int			nb_cara_read;
 	static char	*temp;
-
-	// printf("-----------TEST 3-----------\n");
 	if (temp)
 	{
-		line = ft_strdup(temp);
-		free(temp);
+		printf(" --temp 2 %s\n", temp);
+		line = check_buffer(nb_cara_read, temp, line, &temp);
+		if (temp)
+
+		// line = ft_strdup(temp);
+		// free(temp);
 	}
 	nb_cara_read = BUFFER_SIZE;
 	while (nb_cara_read > 0 && nb_cara_read)
 	{
-		// printf("-----------TEST 4-----------\n");
 		nb_cara_read = read(fd, buffer, BUFFER_SIZE);
 		if (nb_cara_read <= 0)
 		{
@@ -74,13 +67,14 @@ static char	*read_line(int fd, char *buffer, char *line)
 			return (NULL);
 		}
 		printf(" --nb_cara_read : %d\n", nb_cara_read);
-		check_buffer(nb_cara_read, buffer);
-		// check_buffer(nb_cara_read, buffer, temp, line);
-		line = ft_strjoin(line, ft_substr(buffer, 0, nb_cara_read));
+		printf(" --line 00 %s\n", line);
+		line = check_buffer(nb_cara_read, buffer, line, &temp);
+		printf(" --line %s\n", line);
+		if (temp)
+			return (line);
 		if (nb_cara_read < BUFFER_SIZE)
 			nb_cara_read = 0;
 	}
-	// printf("-----------TEST 6-----------\n");
 	return (line);
 }
 
@@ -100,9 +94,7 @@ char	*get_next_line(int fd)
 		free(buffer);
 		return (NULL);
 	}
-	// printf("-----------TEST 7-----------\n");
 	line = read_line(fd, buffer, line);
-	// printf("-----------TEST 8-----------\n");
 	free(buffer);
 	return (line);
 }
